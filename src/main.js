d43e260c8d52c814b737d25c1c6c00f81fb2d1d3
George Chung <Gerhut@GMail.com>
2016-12-08 14:33:58 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import App from './App'
+
+Vue.use(Vuex)
+Vue.use(VueRouter)
+Vue.prototype.$http = axios.create({
+  baseURL: '//pokalc-c55f9.firebaseio.com/'
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
