200ddfd6af3a87b172d83c8bc5e1d8872040f766
Dafrok <o.o@mug.dog>
2016-12-15 14:01:07 +0800

    Bulma Tiles

---
+++ src/components/bulma/grid/Tiles.vue
@@ -0,0 +1,41 @@
+<template>
+.tile(:class="rootClasses")
+  slot
+</template>
+<script>
+export default {
+  props: {
+    ancestor: {
+      type: Boolean,
+      default: false
+    },
+    parent: {
+      type: Boolean,
+      default: false
+    },
+    child: {
+      type: Boolean,
+      default: false
+    },
+    vertical: {
+      type: Boolean,
+      default: false
+    },
+    size: {
+      type: [String, Number]
+    },
+  },
+  computed: {
+    rootClasses () {
+      const {ancestor, parent, child, vertical, size} = this
+      return {
+        'is-ancestor': ancestor,
+        'is-parent:': parent,
+        'is-child': child,
+        'is-vertical': vertical,
+        [`is-${size}`]: size
+      }
+    }
+  }
+}
+</script>
